a397c6b3d4a31aa35e4e38dcf0f5a9260ff3d381
owenh1 <owenh1@students.cs.ubc.ca>
2025-01-12 11:38:59 -0800

    add chat system with bot response

--- app.py
+++ app.py
@@ -1,4 +1,43 @@
 import streamlit as st
+import time
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+st.title("Study Technique Helper Bot")
+
+# function to generate responses
+# TODO: add logic to call utils/chatbot.py
+def generate_response(prompt):
+    # basic response for right now
+    response = "Bot is thinking..."
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.08)
+
+# initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "How can I help you with your studies?"}]
+
+
+# display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# react to user input
+if prompt := st.chat_input("Chat"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        # st.markdown(response)
+        response = st.write_stream(generate_response(prompt))
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
